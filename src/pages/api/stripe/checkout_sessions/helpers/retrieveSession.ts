f3fb6be1da6876d7d8db7efbbd91fe90e89bde5c
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2024-09-03 09:46:30 +0300

    .

--- src/pages/api/stripe/checkout_sessions/helpers/retrieveSession.ts
+++ src/pages/api/stripe/checkout_sessions/helpers/retrieveSession.ts
@@ -2,15 +2,15 @@ import { stripeCli } from "@/server/stripe/stripe";
 import type Stripe from "stripe";
 
 export const retrieveSession = async (sessionId: string) => {
-    const session: Stripe.Checkout.Session =
-      await stripeCli.checkout.sessions.retrieve(sessionId, {
-        expand: [
-          // 'payment_intent',
-          "shipping_cost.shipping_rate",
-          "line_items",
-          "customer",
-          "line_items.data.price.product",
-        ],
-      });
+  const session: Stripe.Checkout.Session =
+    await stripeCli.checkout.sessions.retrieve(sessionId, {
+      expand: [
+        // 'payment_intent',
+        "shipping_cost.shipping_rate",
+        "line_items",
+        "customer",
+        "line_items.data.price.product",
+      ],
+    });
   return session;
-}
+};
