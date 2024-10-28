5d30829d9d6c608e41cc0ee7aee5c90a9e619ad1
kb <kb@admins-MacBook-Pro.local>
2024-10-28 02:18:03 +0200

    .

--- src/pages/api/github/update-file.ts
+++ src/pages/api/github/update-file.ts
@@ -1,46 +1,48 @@
-import { env } from "@/env";
-import commitAndPushToMaster from "@/server/github/commitAndPushToMaster";
-import { type NextApiRequest, type NextApiResponse } from "next";
+// import { env } from "@/env";
+// import commitAndPushToMaster from "@/server/github/commitAndPushToMaster";
+// import { type NextApiRequest, type NextApiResponse } from "next";
 
-type Payload = {
-  secret: string;
-  filePath: string;
-  contents: string;
-  commitMessage?: string;
-};
+// type Payload = {
+//   secret: string;
+//   filePath: string;
+//   contents: string;
+//   commitMessage?: string;
+// };
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: "10mb",
-    },
-    maxDuration: 10,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: {
+//       sizeLimit: "10mb",
+//     },
+//     maxDuration: 10,
+//   },
+// };
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const oops = () => res.status(405).send({ message: "Something went wrong" });
+// async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const oops = () => res.status(405).send({ message: "Something went wrong" });
 
-  if (req.method !== "POST") return oops();
+//   if (req.method !== "POST") return oops();
 
-  const { secret, filePath, contents, commitMessage } = req.body as Payload;
+//   const { secret, filePath, contents, commitMessage } = req.body as Payload;
 
-  if (secret !== env.SENSITIVE_CRUD_SECRET || !filePath || !contents)
-    return oops();
+//   if (secret !== env.SENSITIVE_CRUD_SECRET || !filePath || !contents)
+//     return oops();
 
-  try {
-    const { success } = await commitAndPushToMaster(
-      filePath,
-      contents,
-      commitMessage,
-    );
-    if (!success)
-      throw Error("github/uplaod-file commitAndPushToMaster failed");
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false });
-  }
-}
+//   try {
+//     const { success } = await commitAndPushToMaster(
+//       filePath,
+//       contents,
+//       commitMessage,
+//     );
+//     if (!success)
+//       throw Error("github/uplaod-file commitAndPushToMaster failed");
+//     res.status(200).json({ success: true });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ success: false });
+//   }
+// }
 
-export default handler;
+// export default handler;
+
+export default {};
--- src/pages/api/github/update-images.ts
+++ src/pages/api/github/update-images.ts
@@ -1,48 +1,50 @@
-import { env } from "@/env";
-import commitImagesToRepo from "@/server/github/update-image";
-import { getFilesFromUrls } from "@/server/utils.server";
-import { type NextApiRequest, type NextApiResponse } from "next";
+// import { env } from "@/env";
+// import commitImagesToRepo from "@/server/github/update-image";
+// import { getFilesFromUrls } from "@/server/utils.server";
+// import { type NextApiRequest, type NextApiResponse } from "next";
 
-type ImgUrl = string;
-type FileName = string;
+// type ImgUrl = string;
+// type FileName = string;
 
-type Payload = {
-  secret: string;
-  imgUrls: Record<FileName, ImgUrl>;
-};
+// type Payload = {
+//   secret: string;
+//   imgUrls: Record<FileName, ImgUrl>;
+// };
 
-export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: "10mb",
-    },
-    maxDuration: 10,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: {
+//       sizeLimit: "10mb",
+//     },
+//     maxDuration: 10,
+//   },
+// };
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const oops = () => res.status(405).send({ message: "Something went wrong" });
+// async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   const oops = () => res.status(405).send({ message: "Something went wrong" });
 
-  if (req.method !== "POST") return oops();
+//   if (req.method !== "POST") return oops();
 
-  const { secret, imgUrls } = req.body as Payload;
+//   const { secret, imgUrls } = req.body as Payload;
 
-  if (secret !== env.SENSITIVE_CRUD_SECRET) return oops();
+//   if (secret !== env.SENSITIVE_CRUD_SECRET) return oops();
 
-  try {
-    const base64s = await getFilesFromUrls(imgUrls);
+//   try {
+//     const base64s = await getFilesFromUrls(imgUrls);
 
-    if (!base64s) throw Error("Failed to get base64s from imgUrls");
+//     if (!base64s) throw Error("Failed to get base64s from imgUrls");
 
-    const { success } = await commitImagesToRepo(base64s);
+//     const { success } = await commitImagesToRepo(base64s);
 
-    if (!success) throw Error("github/upload-images commitImagesToRepo failed");
+//     if (!success) throw Error("github/upload-images commitImagesToRepo failed");
 
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ success: false });
-  }
-}
+//     res.status(200).json({ success: true });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).json({ success: false });
+//   }
+// }
 
-export default handler;
+// export default handler;
+
+export default {};
