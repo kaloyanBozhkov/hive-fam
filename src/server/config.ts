a38ed6e2aedb34bc4405aed158867aa68cca91b7
kb <kb@admins-MBP.lan>
2024-12-18 00:08:10 +0200

    .

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,6 +1,6 @@
 // TODO see caching and add domain name to db
 export const DOMAIN_CONFIG = {
-  "hive-family.com": "fa014162-757e-45b0-a46a-2335c06c09c0",
-  "kempwestent.com": "7380adac-fd46-4daa-a8bf-f5bde81bf67f",
+  "www.hive-family.com": "fa014162-757e-45b0-a46a-2335c06c09c0",
+  "www.kempwestent.com": "7380adac-fd46-4daa-a8bf-f5bde81bf67f",
   localhost: "7380adac-fd46-4daa-a8bf-f5bde81bf67f",
 };
