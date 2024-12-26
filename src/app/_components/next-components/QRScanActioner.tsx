505f256bf69ee51500116e3d2d79da554ebc2514
kb <kb@admins-MBP.lan>
2024-12-26 05:10:34 +0200

    .

--- src/app/_components/next-components/QRScanActioner.tsx
+++ src/app/_components/next-components/QRScanActioner.tsx
@@ -5,10 +5,12 @@ import QRScan from "../organisms/QRScanner.organism";
 export const QRScanActioner = () => {
   return (
     <QRScan
-      onDecodedQR={(url) => (window.location.href = url)}
+      onDecodedQR={(url) => {
+        const to = url.startsWith("https://") ? url : `https://${url}`;
+        window.location.href = to;
+      }}
       doneChildren={<p>Scan successful. Redirecting...</p>}
       validateDecodedString={async (str) => {
-        debugger;
         if (!(str.includes("http") || str.includes("www."))) return false;
         if (!str.includes("/validate/")) return false;
         return true;
