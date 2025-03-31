4c6380f5ab9f6909db8e9414feb6f57cd643d7a2
koko <admin@koko.local>
2025-03-31 12:45:52 +0300

    .

--- src/server/config.ts
+++ src/server/config.ts
@@ -4,6 +4,7 @@ export const DOMAIN_CONFIG = {
   "www.kempwestent.com": "7380adac-fd46-4daa-a8bf-f5bde81bf67f",
   "www.urbanspotlight.eu": "a01d4e13-8168-4f4e-9ad6-342acc9e167d",
   "www.eventrave.com": "b01d4e13-8168-4f4e-9ad6-342acc9e167b",
+  "www.naslqpo.com": "fb0c634b-eb4d-4880-8321-a1dece7944ea",
   localhost: "b01d4e13-8168-4f4e-9ad6-342acc9e167b", //"a01d4e13-8168-4f4e-9ad6-342acc9e167d", // "8914a26d-c7b9-4aba-8783-fcafa0eb93c7",
   "192.168.1.12": "a01d4e13-8168-4f4e-9ad6-342acc9e167d", // "8914a26d-
 };
