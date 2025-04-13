79be1941bba8db36cc595dc5a3e99568a5ccce43
koko <admin@koko.local>
2025-04-14 00:39:18 +0300

    .

--- src/app/_components/layouts/Carrier.layout.tsx
+++ src/app/_components/layouts/Carrier.layout.tsx
@@ -42,7 +42,7 @@ export default function CarrierLayout({
         <ScriptsForTranslation />
       </head>
       <body className="relative">
-        <LanguageSwitcher />
+        <LanguageSwitcher defaultLanguage={org.default_language} />
         <MainPageLoader>
           <TRPCReactProvider>
             <AOS>
