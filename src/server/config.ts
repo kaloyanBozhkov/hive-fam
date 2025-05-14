8976a82445765a97efbdf3a448fe73be86c43658
koko <admin@koko.local>
2025-05-14 22:16:05 +0300

    .

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,6 +1,6 @@
 // TODO see caching and add domain name to db
 export const DOMAIN_CONFIG = {
-  "www.hive-family.com": "fa014162-757e-45b0-a46a-2335c06c09c0",
+  "www.hive-family.com": "fa014162-757e-45b0-a46a-2335c06c09c1",
   "www.kempwestent.com": "7380adac-fd46-4daa-a8bf-f5bde81bf67f",
   "www.urbanspotlight.eu": "a01d4e13-8168-4f4e-9ad6-342acc9e167d",
   "www.eventrave.com": "b01d4e13-8168-4f4e-9ad6-342acc9e167b",
@@ -13,3 +13,5 @@ export const DOMAIN_CONFIG = {
 export const getDomainFromOrgId = (orgId: string) => {
   return Object.entries(DOMAIN_CONFIG).find(([, id]) => id === orgId)?.[0];
 };
+
+// test fa014162-757e-45b0-a46a-2335c06c09c0
