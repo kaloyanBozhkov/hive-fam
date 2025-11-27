5ee3f8eef9380776de1cdb9e62397f9504e78403
Kaloyan Bozhkov <your-email@example.com>
2025-11-27 19:48:20 +0700

    .

---
+++ src/pages/api/health/db.ts
@@ -0,0 +1,16 @@
+import { db } from "@/server/db";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  try {
+    await db.$queryRaw`SELECT 1`;
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ error: "Database connection failed", domain: req.headers.host });
+  }
+  res.status(200).json({ message: "OK" });
+}
